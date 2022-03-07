dc2b52a75b8bf551bf100ecc30d47b08be330736
rosenjcb <rosenjcb@gmail.com>
2022-03-06 20:41:18 -0800

    Remove assetPrefix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  assetPrefix: '/rosenjcb',
+  assetPrefix: '/',
   reactStrictMode: true,
   images: {
     loader: 'akamai',
