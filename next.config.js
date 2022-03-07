54dff293a802940e4045f68e21162f49e4d551bb
rosenjcb <rosenjcb@gmail.com>
2022-03-06 19:56:41 -0800

    Another deploy setup push

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
   reactStrictMode: true,
   images: {
     loader: 'akamai',
